5d78091dce77e065527cb9ef269d036413c11dc9
Josh Rowe <josh@s-block.com>
2015-02-21 16:14:29 +0000

    add to pypi

--- setup.py
+++ setup.py
@@ -9,26 +9,26 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name="python_api_client",
+    name='python_api_client',
     version='.'.join(str(v) for v in __version__),
-    author="Josh Rowe",
-    author_email="josh@s-block.com",
-    description="Python client for REST api.",
-    license="BSD",
-    keywords="python REST api client",
-    url="",
+    author='Josh Rowe',
+    author_email='josh@s-block.com',
+    description='Python client for REST api.',
+    license='BSD',
+    keywords='python REST api client',
+    url='',
     packages=find_packages(exclude=['tests']),
     long_description=read('README.md'),
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        'Development Status :: 3 - Alpha',
         'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-        "License :: OSI Approved :: MIT LICENSE",
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
     ],
     install_requires=[
-        "requests >= 2.3.0",
-        "six >= 1.7.3",
-        "pytz >= 2013b",
-        "python-dateutil >= 2.2",
+        'requests >= 2.3.0',
+        'six >= 1.7.3',
+        'pytz >= 2013b',
+        'python-dateutil >= 2.2',
     ]
 )
